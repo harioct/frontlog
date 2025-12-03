8a9222800665b4bcea9d1114f3d8559b07deacba
harioct <octavianhari@gmail.com>
2025-12-03 20:49:53 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tugas-mki-frontlog/", // <--- GANTI INI SESUAI NAMA REPO GITHUB
+  // GANTI 'gamevault' dengan nama repository GitHub kamu yang sebenarnya
+  // Pastikan diawali dan diakhiri dengan tanda garis miring '/'
+  base: "/frontlog/", 
 })
