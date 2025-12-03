b660b0e8b7954fdac07feae51d67c1bd8922b3d1
harioct <octavianhari@gmail.com>
2025-12-03 21:39:22 +0800

    path assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ganti baris ini agar sesuai nama repo di error log kamu:
-  base: "/tugas-mki-frontlog/", 
+  base: './', // <--- Ganti jadi titik garis miring (artinya: relative path)
 })
