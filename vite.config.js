aab89b21142e13287d6950f3f37fb58e965a2493
harioct <octavianhari@gmail.com>
2025-12-03 22:03:45 +0800

    relative path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Pastikan nama repo ini SAMA PERSIS dengan yang ada di URL GitHub kamu
-  base: "/tugas-mki-frontlog/", 
+  base: './', // <--- PENTING: Gunakan titik garis miring
 })
